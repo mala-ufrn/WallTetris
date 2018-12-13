b85964c5c29d7300e477f43f9f27944c98c0ecbd
Joel Felipe <joelfelipe07@gmail.com>
2018-12-12 21:32:12 -0300

    Add messages and save highscore

--- src/scenes/endless_mode.cpp
+++ src/scenes/endless_mode.cpp
@@ -19,7 +19,8 @@ EndlessMode::EndlessMode(SceneMaster* sceneMaster, irrklang::ISoundEngine* sound
   this->sceneMaster = sceneMaster;
   this->soundEngine = soundEngine;
 
-  highScore = 0;
+  std::ifstream log("src/score.txt", std::ios_base::in);
+  log >> highScore;
 
   winOrigDims = win_dimentions;
   scrFactor = scrFact;
@@ -361,13 +362,30 @@ void EndlessMode::draw() {
     sansPsx64->renderLeft("\\C: roll", 430.0f, 180.0f, 0.35f, glm::vec3(0.6f));
     sansPsx64->renderLeft("\\G: speed up", 430.0f, 150.0f, 0.35f, glm::vec3(0.6f));
     sansPsx64->renderLeft("\\A: pause", 430.0f, 120.0f, 0.35f, glm::vec3(0.6f));
+    if (paused){
+      sansKey64->renderLeft("Pause", 180.0f, 300.0f, 0.7f, glm::vec3(0.8f));
+      sansKey64->renderLeft("Press \\C to give up", 148.0f, 275.0f, 0.35f, glm::vec3(0.8f));
+    }
+    if (!playing){
+      sansKey64->renderLeft("Game Over", 130.0f, 300.0f, 0.7f, glm::vec3(0.8f));
+      sansKey64->renderLeft("Press \\X to Menu", 50.0f, 300.0f, 0.35f, glm::vec3(0.8f));
+    }
   } else {
     sansKey64->renderLeft("\\A or \\a: move left", 430.0f, 210.0f, 0.35f, glm::vec3(0.6f));
     sansKey64->renderLeft("\\D or \\d: move right", 430.0f, 180.0f, 0.35f, glm::vec3(0.6f));
     sansKey64->renderLeft("\\W or \\w: roll", 430.0f, 150.0f, 0.35f, glm::vec3(0.6f));
     sansKey64->renderLeft("\\S or \\s: speed up", 430.0f, 120.0f, 0.35f, glm::vec3(0.6f));
     sansKey64->renderLeft("\\P or \\e: pause", 430.0f, 90.0f, 0.35f, glm::vec3(0.6f));
+    if (paused){
+      sansKey64->renderLeft("Pause", 182.0f, 300.0f, 0.7f, glm::vec3(0.8f));
+      sansKey64->renderLeft("Press \\e to give up", 148.0f, 275.0f, 0.35f, glm::vec3(0.8f));
+    }
+    if (!playing){
+      sansKey64->renderLeft("Game Over", 128.0f, 300.0f, 0.7f, glm::vec3(0.8f));
+      sansKey64->renderLeft("Press \\f to Menu", 140.0f, 275.0f, 0.35f, glm::vec3(0.8f));
+    }
   }
+
 }
 
 void EndlessMode::startGame() {
